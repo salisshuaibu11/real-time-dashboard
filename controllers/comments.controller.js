7218ef80b184ef1ee5785478a95b73c9322eac4c
salisu <salisshuaibu11@gmail.com>
2021-08-16 23:11:22 +0000

    Initial commit and also created comments controller

---
+++ controllers/comments.controller.js
@@ -0,0 +1,23 @@
+var comments = [{
+    user: 'Batman',
+    comment: 'Great post!'
+}, {
+    user: 'Robin',
+    comment: 'Interesting ideas...'
+}, {
+    user: 'Joker',
+    comment: 'Thanks, Batman!'
+}, {
+    user: 'Bruce Wayne',
+    comment: 'I agree with Batman'
+}];
+
+module.exports = (socket) => {
+  // Recent Comments
+  for (var i = 0; i < comments.length; i++) {
+    socket.emit("comment.add", comments[i]);
+    socket.emit("comments.count", {
+      count: i + 1
+    });
+  }
+};
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express"),
+      app = express(),
+      http = require("http"),
+      socketIO = require("socket.io"),
+      server, io;
+
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+server = http.Server(app);
+server.listen(5000);
+
+io = socketIO(server);
+
+io.on("connection", (socket) => {
+  const controllers = ["comments", "posts"];
+  for (var i = 0; i < controllers.length; i++) {
+    require(`./controllers/${controllers[i].controller}`)(socket);
+  }
+});
